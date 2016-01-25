80f00b8577bac64a8848d8149f3c3c6d106a7958
Akanksha Vyas <vyasakanksha@gmail.com>
2016-01-25 19:17:04 +0530

    Added a SVM predictor
    Added another prediction algorithm that uses support vector
    machines to predict the cusisine. Sadly this is broken right now

--- predictor.py
+++ predictor.py
@@ -1,10 +1,18 @@
 import time
+from sklearn import datasets, svm, metrics
+import numpy as np
 
+
+# Checks to see how many ingredients in the recipe match the 
+# ingredients in a particular cuisine. If the current recipe
+# is a proper subset of any cuisine, we have a winner. If not, 
+# we go with the cuisine that has the maximum common ingredients
 def slow_predictor(test, cuisines):
+   output = []
    for t in test:
       counts = {}
-      for i, k in cuisines.items():
-         c = len(set(k) & set(t.ingr))
+      for i, v in cuisines.items():
+         c = len(set(v) & set(t.ingr))
          if len(t.ingr) - c is 0:
             t.name = i
          else: 
@@ -12,4 +20,32 @@ def slow_predictor(test, cuisines):
 
       if t.name is None:
          t.name = min(counts.items(), key=lambda x: x[1])[0]
-            
+      output.append([t.idf, t.name]) 
+   return output
+
+# We convert the recipies of the training set into a binary matrix and
+# train a SVM on that matrix with the cuisines as lables. Then we 
+# use the SVM to predict the cuisines from the testing set. 
+
+#Currently this is broken :(
+def predictor(test, train, ingredients):
+   output = []
+   sample = np.zeros((len(train),len(ingredients)))
+   names = [None] * len(train)
+
+   for t in range(len(train)):
+       names[t] = train[t].name
+       for i in train[t].ingr:
+           sample[t][ingredients.index(i)] = 1
+   
+   classifier = svm.SVC(gamma=0.001)
+  
+   # These keep crashing :'( 
+
+   #classifier.fit(sample, names)
+   #predicted = classifier.predict(data_test)
+   for t in range(len(test)):
+       output.append([test[t].idf, predicted[t]]) 
+   return output
+
+
